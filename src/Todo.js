1424ec2bb4349b499712ced6acb18416ec84f792
mattchupp <mattlchupp@gmail.com>
2020-05-21 22:33:35 -0500

    item creation working

--- src/Todo.js
+++ src/Todo.js
@@ -2,11 +2,49 @@ import React, { Component } from 'react';
 
 
 class Todo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      itemInput: '',
+      todoList: []
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({itemInput: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const newItem = {
+      id: Date.now(),
+      input: this.state.itemInput
+    }
+
+    this.setState(state => ({
+      todoList: state.todoList.concat(newItem),
+      itemInput: ''
+    }));
+  }
 
   render() {
     return (
       <div>
-        Hello!
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' value={this.state.itemInput} onChange={this.handleChange} placeholder={this.state.itemInput} />
+        </form>
+        <br />
+
+        <br />
+        <ul>
+          {this.state.todoList.map(item => (
+            <li key={item.id}>{item.input}</li>
+          ))}
+        </ul>
       </div>
     );
   }
